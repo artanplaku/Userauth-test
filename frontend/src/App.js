d221ff315b5c1bca08d903b00856de02d0740525
Artan <artanpl03@gmail.com>
2022-09-21 21:50:44 -0400

    added signup and route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
 import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" exact element={<Login />} />
-      <Route path="/" element={<Navigate replace to="/login" />} />
+      <Route path="/login"  element={<Login />} />
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/signup"  element={<SignUp />} />
     </Routes>
   );
 }
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -6,8 +6,41 @@
 const SignUp = () =>{
 
     return (
-        <div className="container">
+        <div className="signup_container">
+            <div className="signup_form">
+            <form className="form_container">
+                <h1>Create Account</h1>
+                <input
+                    type="text"
+                    placeholder="First Name"
+                    name="firstName"
+                    // onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Last Name"
+                    name="lastName"
+                    // onChange={handleChange}
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    // onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    // onChange={handleChange}
+                />
+                <button type="submit" className="button">
+							Sing Up
+						</button>
+					
 
+            </form>
+            </div>  
         </div>
     )
 }
